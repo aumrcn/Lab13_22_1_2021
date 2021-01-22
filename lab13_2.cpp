d81828d22818085492bf425e1db1b4ed0113b731
aumrcn <aumkung20728@gmail.com>
2021-01-22 19:59:25 +0700

    lab13_2.cpp completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -19,3 +19,44 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool bobo[N][N]){
+    int i,j;
+    for(i = 0; i<N; ++i){
+        for(j = 0 ; j<N; ++j){
+            cout << bobo[i][j] << " ";
+        }
+    cout << endl;
+    }
+    return;
+}
+
+void inputMatrix(double bobo[][N]){
+    int i,j;
+    for(i = 0 ; i < N ; ++i){
+        cout << "Row " << i+1 << ": ";
+        for(j = 0 ; j < N ; ++j){
+            cin >> bobo[i][j];
+        }    
+    }
+    return;
+}
+
+void findLocalMax(const double arr1[][N], bool arr2[][N]){
+    for(int i = 0 ; i < N ; ++i){
+        for(int j = 0 ; j < N ; ++j){
+            arr2[i][j] = 0;
+        }
+    }
+    for(int i = 1 ; i < N-1 ; ++i){
+        for(int j = 1 ; j < N-1 ; ++j){
+            if(arr1[i][j] >= arr1[i - 1][j] && 
+            arr1[i][j] >= arr1[i][j + 1] && 
+            arr1[i][j] >= arr1[i + 1][j] && 
+            arr1[i][j] >= arr1[i][j - 1]){
+                arr2[i][j] = 1;
+            }
+        }
+    }
+    return;
+}
